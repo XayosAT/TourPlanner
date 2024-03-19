a82e3876ba582c7c00cd253fe801c36b1b3d488f
XayosAT <christioph-leopold@gmx.at>
2024-03-19 17:28:19 +0100

    RouteViewModel Draft

---
+++ TourPlanner/ViewModels/RouteViewModel.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace TourPlanner.ViewModels;
+
+
+public class RouteViewModel : INotifyPropertyChanged
+{
+    
+    public ICommand ShowAddLogFormCommand { get; set; }
+    public ICommand AddLogCommand { get; set; }
+    public ICommand CancelAddLogCommand { get; set; }
+    private bool _showAddLogForm;
+    
+    
+    public RouteViewModel()
+    {
+        ShowAddLogFormCommand = new RelayCommand(ShowAddLogFormAction);
+    }
+    
+    private void ShowAddLogFormAction()
+    {
+        this.ShowAddLogForm = true;
+    }
+    
+    
+    public bool ShowAddLogForm
+    {
+        get => this._showAddLogForm;
+        set
+        {
+            this._showAddLogForm = value;
+            this.OnPropertyChanged();
+        }
+    }
+    
+    
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
